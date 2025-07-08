dfabcfab5cb5bfda5bc3ea860e6b4a9f23df8894
Loidu-kun <194797489+Loidu-kun@users.noreply.github.com>
2025-07-08 15:00:09 +0800

    Math
    Math in JS

---
+++ math/index.js
@@ -0,0 +1,43 @@
+// Math = built-in object that provides a collection of properties and methods
+/*
+Math.PI()
+Math.E()
+Math.round()
+Math.floor()
+Math.ceil()
+Math.trunc()
+Math.pow()
+Math.sqrt()
+Math.log()
+Math.sin();
+Math.cos()
+Math.tan()
+Math.abs()
+Math.sign()
+Math.max()
+Math.min()
+*/
+
+console.log(Math.PI);
+console.log(Math.E);
+
+let x = 3;
+let y = 2;
+let z = 1;
+
+//z = Math.round(x);
+//z = Math.floor(x);
+//z  = Math.ceil(x);
+//z  = Math.trunc(x);
+//z  = Math.pow(x, y);
+//z  = Math.sqrt(100);
+//z = Math.log(x);
+//z = Math.sin(x);
+//z = Math.cos(x);
+//z = Math.tan(x);
+//z = Math.abs(x);
+//z = Math.sign(x);
+//let ans = Math.max(x, y, z);
+let ans = Math.min(x, y, z);
+
+console.log(ans);
