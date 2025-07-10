1f8ae7b99348e0435b52c5ed826d0b999e2cf162
Loidu-kun <194797489+Loidu-kun@users.noreply.github.com>
2025-07-10 19:16:57 +0800

    Ternary Operator
    It is like a shortcut for the condition statement in JS

---
+++ ternaryOperator/index.js
@@ -0,0 +1,24 @@
+/*
+Ternary Operator = a shortcut to if() and else () statements
+                    helps to assign a variable based on a condition
+                    condition ? condeIfTrue : codeIfFalse;
+
+*/
+
+let age = 10;
+let message = age >= 18 ? "You're an adult" : "You're a minor";
+console.log(message);
+
+
+let time = 11;
+let greeting = time < 12  ? "Good Morning" : "Good Afternoon";
+console.log(greeting);
+
+let isStudent = false;
+let stats = isStudent ? "You're a Student" : "You're not Student";
+console.log(stats);
+
+
+let amount = 99;
+let discount    = amount >= 100 ? 10 : 0;
+console.log(`Your total is $${amount - amount * (discount/100)}`)
