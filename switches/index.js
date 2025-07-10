19f735f0ee55a30a6c2ed649ed880ff7ee1bc65a
Loidu-kun <194797489+Loidu-kun@users.noreply.github.com>
2025-07-10 20:39:01 +0800

    switch
    Switches in JS

---
+++ switches/index.js
@@ -0,0 +1,50 @@
+/*
+    Switch = can be a efficient replacement to many else if statements
+*/
+
+let day = "meow";
+
+switch(day){
+        case 1:
+            console.log("Monday");
+            break; //  to break out the switch if condition is already achieved
+        case 2:
+            console.log("Tuesday");
+            break;
+        case 3:
+            console.log("Wednesday");
+            break;
+        case 4:
+            console.log("Thursday");
+            break;
+        case 5:
+            console.log("Friday");
+            break;
+        case 6:
+            console.log("Saturday");
+            break;
+        case 7:
+            console.log("Sunday");
+            break;
+        default:
+            console.log(`${day} is not a day`);
+}
+
+let score = 69;
+let grade;
+
+switch(true){
+    case score >= 90:
+        grade = "A";
+        break;
+    case score >= 80:
+        grade = "B";
+        break;
+    case score >= 70:
+        grade = "C";
+        break;
+    default:
+        grade = "FAILED"        
+}
+
+console.log(`Your remarks is ${grade}`);
--- ternaryOperator/index.js
+++ ternaryOperator/index.js
@@ -9,7 +9,6 @@ let age = 10;
 let message = age >= 18 ? "You're an adult" : "You're a minor";
 console.log(message);
 
-
 let time = 11;
 let greeting = time < 12  ? "Good Morning" : "Good Afternoon";
 console.log(greeting);
@@ -18,7 +17,6 @@ let isStudent = false;
 let stats = isStudent ? "You're a Student" : "You're not Student";
 console.log(stats);
 
-
-let amount = 99;
+let amount = 100;
 let discount    = amount >= 100 ? 10 : 0;
 console.log(`Your total is $${amount - amount * (discount/100)}`)
