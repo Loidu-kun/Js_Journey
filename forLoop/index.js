68ef7777ba750ba7b46e45ddbbfae96c9e1d1f75
Loidu-kun <194797489+Loidu-kun@users.noreply.github.com>
2025-08-03 21:36:17 +0800

    Loops
    For Loops in JS

---
+++ forLoop/index.js
@@ -0,0 +1,22 @@
+/*
+
+for loop = repeat some code a LIMITED amount of timees
+
+*/
+
+for(let i = 10; i > 0; i-=2){
+    console.log(i);
+}
+console.log("Api new year!!");
+
+
+for(let j = 1; j <= 20; j++){
+
+    if(j == 13){
+        continue;
+    }
+    else{
+    console.log(j);
+    }
+    
+}
