1444a036ff8914cd2b7628428822bd58ba3d3798
Loidu-kun <194797489+Loidu-kun@users.noreply.github.com>
2025-08-21 22:26:48 +0800

    .reducemethod
    showing the output of .reduce() in html

--- reduce/script.js
+++ reduce/script.js
@@ -5,24 +5,33 @@
 */
 
 const prices = [12, 14, 16, 20, 28];
-
 const total = prices.reduce(sum);
 
 console.log(`$${total.toFixed(2)}`);//Noted .toFixed is used for adding decimal
 
 function sum(nEw, old){
     return nEw + old;
-}
+} 
 
 
 const grades = [70, 75, 80, 85, 90, 95];
 
 const maxValue = grades.reduce(getMax);
 const minValue = grades.reduce(getMin);
-
+const passingGrade = document.getElementById("passing");
+const highGrade = document.getElementById("highest");
+const listGrade = document.getElementById("gradeList");
 
 console.log(maxValue);
 console.log(minValue);
+
+listGrade.textContent = "Here are the list of grades " + grades.join(", ");
+highGrade.textContent = "The Highest Grade in this class is " + maxValue;
+passingGrade.textContent = "The lowest Grade in this class is " + minValue;
+
+for(let i = 0; i < grades.length; i++){
+     console.log(grades[i]);
+}
 
 function getMax(x, y){
     return Math.max(x, y);
@@ -31,3 +40,5 @@ function getMax(x, y){
 function getMin(x, y){
     return Math.min(x, y);
 }
+
+
